fc1a4eb4648f496fb1f258b339a4bcae8f42d1c7
ramaalfin <ramaalfin7@gmail.com>
2026-03-12 08:50:23 +0700

    feat: remove SettingGroupCreatePage and its corresponding route from the application.

--- src/router.tsx
+++ src/router.tsx
@@ -15,7 +15,6 @@ import SettingPage from "./features/setting/pages/SettingPage";
 import SettingAccountPage from "./features/setting/pages/SettingAccountPage";
 import SettingGroupPage from "./features/setting/pages/SettingGroupPage";
 import SettingGroupDetailPage from "./features/setting/pages/SettingGroupDetailPage";
-import SettingGroupCreatePage from "./features/setting/pages/SettingGroupCreatePage";
 import CreateGroupPage from "./features/setting/pages/CreateGroupPage";
 
 const requireAuthLoader = () => {
@@ -102,7 +101,6 @@ const router = createBrowserRouter([
       },
       {
         path: "/home/setting/groups/create",
-        element: <SettingGroupCreatePage />,
         element: <CreateGroupPage />,
         loader: requireAuthLoader,
       },
